136eb2d558406a0c481f95f481bf10e20189cad5
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2024-11-12 15:58:03 +0700

    Update for Pet module'

--- lib/presentation/cart/widgets/cart_item.dart
+++ lib/presentation/cart/widgets/cart_item.dart
@@ -5,13 +5,15 @@ import 'package:flutter_pet_shop_app/core/config/currency_rate.dart';
 import 'package:flutter_pet_shop_app/core/helper/money_format_helper.dart';
 import 'package:flutter_pet_shop_app/core/resources/color_manager.dart';
 import 'package:flutter_pet_shop_app/domain/entities/merchandise_item.dart';
+import 'package:flutter_pet_shop_app/domain/entities/pet.dart';
 import 'package:flutter_pet_shop_app/presentation/cart/cubit/cart_cubit.dart';
 import 'package:flutter_pet_shop_app/presentation/cart/cubit/cart_state.dart';
 import 'package:flutter_pet_shop_app/presentation/widgets/add_button.dart';
+import 'package:flutter_pet_shop_app/presentation/widgets/color_box_from_color_hex.dart';
 import 'package:flutter_pet_shop_app/presentation/widgets/remove_button.dart';
 
 class CartItem extends StatefulWidget {
-  final (int, MerchandiseItem) cartItem;
+  final (int, Object) cartItem;
   const CartItem({super.key, required this.cartItem});
 
   @override
@@ -20,10 +22,12 @@ class CartItem extends StatefulWidget {
 
 class _CartItemState extends State<CartItem> {
   final TextEditingController _quantityController = TextEditingController();
+  bool _isMerchandise = true;
 
   @override
   void initState() {
     super.initState();
+    _isMerchandise = widget.cartItem.$2 is MerchandiseItem;
     _quantityController.text = widget.cartItem.$1.toString();
   }
 
@@ -49,7 +53,9 @@ class _CartItemState extends State<CartItem> {
                   flex: 5,
                   child: Image.network(
                     fit: BoxFit.fill,
-                    widget.cartItem.$2.imageUrl,
+                    _isMerchandise
+                        ? (widget.cartItem.$2 as MerchandiseItem).imageUrl
+                        : (widget.cartItem.$2 as Pet).imageUrl,
                   ),
                 ),
                 Expanded(flex: 1, child: SizedBox()),
@@ -61,12 +67,16 @@ class _CartItemState extends State<CartItem> {
                       children: [
                         BlocBuilder<CartCubit, CartState>(
                             builder: (context, state) {
-                          int indexOfItem = state.cartList.indexWhere(
-                              (element) =>
-                                  element.$2.id == widget.cartItem.$2.id);
+                          int indexOfItem = context
+                              .read<CartCubit>()
+                              .getIndexOfItem(
+                                  itemId: widget.cartItem.$2 is MerchandiseItem
+                                      ? (widget.cartItem.$2 as MerchandiseItem)
+                                          .id
+                                      : (widget.cartItem.$2 as Pet).id);
 
                           return Text(
-                            "${MoneyFormatHelper.formatVNCurrency(widget.cartItem.$2.price * CurrencyRate.vnd)} x ${indexOfItem != -1 ? state.cartList[indexOfItem].$1 : _quantityController.text}",
+                            "${MoneyFormatHelper.formatVNCurrency(_isMerchandise ? (widget.cartItem.$2 as MerchandiseItem).price * CurrencyRate.vnd : (widget.cartItem.$2 as Pet).price * CurrencyRate.vnd)} x ${indexOfItem != -1 ? state.cartList[indexOfItem].$1 : _quantityController.text}",
                             style: TextStyle(
                                 color: AppColor.green,
                                 fontSize: 15,
@@ -77,7 +87,9 @@ class _CartItemState extends State<CartItem> {
                           height: 8,
                         ),
                         Text(
-                          widget.cartItem.$2.name,
+                          _isMerchandise
+                              ? (widget.cartItem.$2 as MerchandiseItem).name
+                              : (widget.cartItem.$2 as Pet).name,
                           style: TextStyle(
                               fontSize: 17, fontWeight: FontWeight.bold),
                           overflow: TextOverflow.ellipsis,
@@ -85,75 +97,93 @@ class _CartItemState extends State<CartItem> {
                         SizedBox(
                           height: 8,
                         ),
-                        Text(
-                          widget.cartItem.$2.weight,
-                          style: TextStyle(color: AppColor.gray, fontSize: 12),
-                        )
+                        _isMerchandise
+                            ? Text(
+                                (widget.cartItem.$2 as MerchandiseItem).weight,
+                                style: TextStyle(
+                                    color: AppColor.gray, fontSize: 12),
+                              )
+                            : ColorBoxFromColorHex(
+                                colorName: (widget.cartItem.$2 as Pet).color,
+                                width: 30,
+                                height: 30)
                       ],
                     )),
-                Expanded(
-                    flex: 2,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        AddButton(onButtonClick: () {
-                          setState(() {
-                            _quantityController.text =
-                                _quantityController.text.isNotEmpty
+                _isMerchandise
+                    ? Expanded(
+                        flex: 2,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            AddButton(onButtonClick: () {
+                              setState(() {
+                                _quantityController.text = _quantityController
+                                        .text.isNotEmpty
                                     ? (int.parse(_quantityController.text) + 1)
                                         .toString()
                                     : "1";
-                          });
-                          context
-                              .read<CartCubit>()
-                              .increaseQuantityOfItem(widget.cartItem.$2.id);
-                        }),
-                        SizedBox(
-                          height: 10,
-                          child: TextField(
-                            controller: _quantityController,
-                            style:
-                                TextStyle(color: AppColor.gray, fontSize: 15),
-                            textAlign: TextAlign.center,
-                            textInputAction: TextInputAction.done,
-                            inputFormatters: <TextInputFormatter>[
-                              FilteringTextInputFormatter.allow(
-                                  RegExp(r'^[1-9][0-9]*'))
-                            ],
-                            decoration: InputDecoration(
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Colors.transparent, width: 0)),
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Colors.transparent, width: 0))),
-                            cursorColor: AppColor.gray,
-                            onChanged: (value) {
-                              setState(() {
-                                _quantityController.text = value;
                               });
-                              if (value.toString().isNotEmpty) {
-                                context.read<CartCubit>().updateCart(
-                                    (int.parse(value), widget.cartItem.$2));
-                              }
-                            },
-                          ),
-                        ),
-                        RemoveButton(onButtonClick: () {
-                          setState(() {
-                            _quantityController.text =
-                                _quantityController.text.isNotEmpty &&
+                              context.read<CartCubit>().increaseQuantityOfItem(
+                                    _isMerchandise
+                                        ? (widget.cartItem.$2
+                                                as MerchandiseItem)
+                                            .id
+                                        : (widget.cartItem.$2 as Pet).id,
+                                  );
+                            }),
+                            SizedBox(
+                              height: 10,
+                              child: TextField(
+                                controller: _quantityController,
+                                style: TextStyle(
+                                    color: AppColor.gray, fontSize: 15),
+                                textAlign: TextAlign.center,
+                                textInputAction: TextInputAction.done,
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.allow(
+                                      RegExp(r'^[1-9][0-9]*'))
+                                ],
+                                decoration: InputDecoration(
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: Colors.transparent,
+                                            width: 0)),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: Colors.transparent,
+                                            width: 0))),
+                                cursorColor: AppColor.gray,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _quantityController.text = value;
+                                  });
+                                  if (value.toString().isNotEmpty) {
+                                    context.read<CartCubit>().updateCart(
+                                        (int.parse(value), widget.cartItem.$2));
+                                  }
+                                },
+                              ),
+                            ),
+                            RemoveButton(onButtonClick: () {
+                              setState(() {
+                                _quantityController.text = _quantityController
+                                            .text.isNotEmpty &&
                                         int.parse(_quantityController.text) > 1
                                     ? (int.parse(_quantityController.text) - 1)
                                         .toString()
                                     : "1";
-                          });
-                          context
-                              .read<CartCubit>()
-                              .decreaseQuantityOfItem(widget.cartItem.$2.id);
-                        })
-                      ],
-                    ))
+                              });
+                              context.read<CartCubit>().decreaseQuantityOfItem(
+                                    _isMerchandise
+                                        ? (widget.cartItem.$2
+                                                as MerchandiseItem)
+                                            .id
+                                        : (widget.cartItem.$2 as Pet).id,
+                                  );
+                            })
+                          ],
+                        ))
+                    : Expanded(flex: 2, child: SizedBox())
               ],
             ),
           )),
