fd05579e21250f4e79d610eb2f27dd0ada4d99cf
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2024-11-13 16:57:50 +0700

    Update for bottom bar

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,7 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_pet_shop_app/core/constants/auth_state_enum.dart';
+import 'package:flutter_pet_shop_app/core/config/config.dart';
+import 'package:flutter_pet_shop_app/core/enum/auth_state_enum.dart';
 import 'package:flutter_pet_shop_app/core/resources/color_manager.dart';
 import 'package:flutter_pet_shop_app/presentation/auth/cubit/auth_cubit.dart';
 import 'package:flutter_pet_shop_app/presentation/auth/cubit/auth_state.dart';
@@ -40,135 +41,120 @@ class _HomePageState extends State<HomePage> {
           child:
               BlocConsumer<HomeCubit, HomeState>(builder: (context, homeState) {
             return SafeArea(
-                child: _isShimmer
-                    ? Shimmer.fromColors(
-                        baseColor: AppColor.green.withOpacity(0.4),
-                        highlightColor: AppColor.gray,
-                        child: Scaffold(
-                          appBar: AppBar(
-                              backgroundColor: AppColor.green,
-                              title: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'greeting',
-                                    style: TextStyle(
-                                        color: AppColor.white,
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.w600),
-                                  ).tr(args: [userName]),
-                                  Container(
-                                    width: 50,
-                                    height: 50,
-                                    decoration: BoxDecoration(
-                                        color: AppColor.white,
-                                        borderRadius:
-                                            BorderRadius.circular(16)),
-                                    child:
-                                        Image.asset("assets/images/coco.png"),
-                                  )
-                                ],
-                              )),
-                          body: CustomScrollView(
-                            slivers: <Widget>[
-                              SliverAppBar(
-                                automaticallyImplyLeading: false,
-                                expandedHeight:
-                                    MediaQuery.of(context).size.height * 0.3,
-                                flexibleSpace: FlexibleSpaceBar(
-                                  background: Image.network(
-                                      "https://firebasestorage.googleapis.com/v0/b/pet-shop-4739c.appspot.com/o/statics%2Fhome_banner.jpg?alt=media&token=ceb24769-74a9-457d-a660-92e99695d2fb"),
-                                ),
-                              ),
-                              SliverToBoxAdapter(
-                                child: Padding(
-                                    padding: EdgeInsets.all(16),
-                                    child: SingleChildScrollView(
-                                      scrollDirection: Axis.vertical,
-                                      child: Column(
-                                        children: [
-                                          OurPetsSection(
-                                            petList: homeState.petList,
-                                          ),
-                                          SizedBox(height: 16),
-                                          AccessoriesSection(
-                                              accessoryList:
-                                                  homeState.accessoryList),
-                                          SizedBox(
-                                            height: 16,
-                                          ),
-                                          FoodsSection(
-                                              foodList: homeState.foodList)
-                                        ],
-                                      ),
-                                    )),
-                              )
-                            ],
-                          ),
-                        ))
-                    : Scaffold(
-                        appBar: AppBar(
-                            backgroundColor: AppColor.green,
-                            title: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
+                bottom: false,
+                child: Scaffold(
+                  appBar: AppBar(
+                      backgroundColor: AppColor.green,
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          _isShimmer
+                              ? Shimmer.fromColors(
+                                  baseColor: AppColor.green.withOpacity(0.4),
+                                  highlightColor: AppColor.gray,
+                                  child: SizedBox(),
+                                )
+                              : Text(
                                   'greeting',
                                   style: TextStyle(
                                       color: AppColor.white,
                                       fontSize: 15,
                                       fontWeight: FontWeight.w600),
                                 ).tr(args: [userName]),
-                                Container(
-                                  width: 50,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                      color: AppColor.white,
-                                      borderRadius: BorderRadius.circular(16)),
-                                  child: Image.asset("assets/images/coco.png"),
-                                )
-                              ],
-                            )),
-                        body: CustomScrollView(
-                          slivers: <Widget>[
-                            SliverAppBar(
-                              automaticallyImplyLeading: false,
-                              expandedHeight:
-                                  MediaQuery.of(context).size.height * 0.3,
-                              flexibleSpace: FlexibleSpaceBar(
-                                background: Image.network(
-                                    "https://firebasestorage.googleapis.com/v0/b/pet-shop-4739c.appspot.com/o/statics%2Fhome_banner.jpg?alt=media&token=ceb24769-74a9-457d-a660-92e99695d2fb"),
-                              ),
-                            ),
-                            SliverToBoxAdapter(
-                              child: Padding(
-                                  padding: EdgeInsets.all(16),
-                                  child: SingleChildScrollView(
-                                    scrollDirection: Axis.vertical,
-                                    child: Column(
-                                      children: [
-                                        OurPetsSection(
+                          Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                                color: AppColor.white,
+                                borderRadius: BorderRadius.circular(16)),
+                            child: Image.asset("assets/images/coco.png"),
+                          )
+                        ],
+                      )),
+                  body: CustomScrollView(
+                    slivers: <Widget>[
+                      SliverAppBar(
+                        automaticallyImplyLeading: false,
+                        expandedHeight:
+                            MediaQuery.of(context).size.height * 0.3,
+                        flexibleSpace: FlexibleSpaceBar(
+                          background: _isShimmer
+                              ? Shimmer.fromColors(
+                                  baseColor: AppColor.green.withOpacity(0.4),
+                                  highlightColor: AppColor.gray,
+                                  child: Container(
+                                    color: AppColor.gray,
+                                    width: double.infinity,
+                                  ))
+                              : Image.network(Config.homeBannerImageUrl),
+                        ),
+                      ),
+                      SliverToBoxAdapter(
+                        child: Padding(
+                            padding: EdgeInsets.all(16),
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.vertical,
+                              child: Column(
+                                children: [
+                                  _isShimmer
+                                      ? Shimmer.fromColors(
+                                          baseColor:
+                                              AppColor.green.withOpacity(0.4),
+                                          highlightColor: AppColor.gray,
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: AppColor.gray,
+                                                borderRadius:
+                                                    BorderRadius.circular(16)),
+                                            width: double.infinity,
+                                            height: 200,
+                                          ))
+                                      : OurPetsSection(
                                           petList: homeState.petList,
                                         ),
-                                        SizedBox(height: 16),
-                                        AccessoriesSection(
-                                            accessoryList:
-                                                homeState.accessoryList),
-                                        SizedBox(
-                                          height: 16,
-                                        ),
-                                        FoodsSection(
-                                            foodList: homeState.foodList)
-                                      ],
-                                    ),
-                                  )),
-                            )
-                          ],
-                        ),
-                      ));
+                                  SizedBox(height: 16),
+                                  _isShimmer
+                                      ? Shimmer.fromColors(
+                                          baseColor:
+                                              AppColor.green.withOpacity(0.4),
+                                          highlightColor: AppColor.gray,
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: AppColor.gray,
+                                                borderRadius:
+                                                    BorderRadius.circular(16)),
+                                            width: double.infinity,
+                                            height: 200,
+                                          ))
+                                      : AccessoriesSection(
+                                          accessoryList:
+                                              homeState.accessoryList),
+                                  SizedBox(
+                                    height: 16,
+                                  ),
+                                  _isShimmer
+                                      ? Shimmer.fromColors(
+                                          baseColor:
+                                              AppColor.green.withOpacity(0.4),
+                                          highlightColor: AppColor.gray,
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: AppColor.gray,
+                                                borderRadius:
+                                                    BorderRadius.circular(16)),
+                                            width: double.infinity,
+                                            height: 200,
+                                          ))
+                                      : FoodsSection(
+                                          foodList: homeState.foodList)
+                                ],
+                              ),
+                            )),
+                      )
+                    ],
+                  ),
+                ));
           }, listener: (context, state) {
             if (state.accessoryList.isNotEmpty && state.foodList.isNotEmpty) {
               setState(() {
