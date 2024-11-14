13bced1e7082e080963eca115adb76f4449adf75
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2024-11-14 13:38:05 +0700

    Add config for height main bottom bar

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  static final String homeBannerImageUrl =
+      "https://firebasestorage.googleapis.com/v0/b/pet-shop-4739c.appspot.com/o/statics%2Fhome_banner.jpg?alt=media&token=ceb24769-74a9-457d-a660-92e99695d2fb";
+  static final String placeHolderMerchandiseImageUrl =
+      "https://firebasestorage.googleapis.com/v0/b/pet-shop-4739c.appspot.com/o/merchandise_images%2Fjosera.png?alt=media&token=5ca61ce3-c3d0-46fe-807b-5947335775ab";
+  static final double mainBottomNavigationBarHeight = 50;
+}
