7770b21f36addcc706e61ef46860bc3202e829dc
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2024-11-20 10:56:02 +0700

    Add config for deep link

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -4,4 +4,5 @@ class AppConfig {
   static final String placeHolderMerchandiseImageUrl =
       "https://firebasestorage.googleapis.com/v0/b/pet-shop-4739c.appspot.com/o/merchandise_images%2Fjosera.png?alt=media&token=5ca61ce3-c3d0-46fe-807b-5947335775ab";
   static final double mainBottomNavigationBarHeight = 50;
+  static final String customUri = "apelion://s.petshop.com";
 }
