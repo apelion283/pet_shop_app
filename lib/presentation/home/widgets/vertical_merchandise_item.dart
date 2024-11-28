cd0d3d99bcd6e30b691f6c6562027ef48e4955d2
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2024-11-28 09:40:13 +0700

    Set max width for item

--- lib/presentation/home/widgets/vertical_merchandise_item.dart
+++ lib/presentation/home/widgets/vertical_merchandise_item.dart
@@ -10,34 +10,37 @@ Widget verticalMerchandiseItem(
       onTap: () {
         onItemClick();
       },
-      child: Card(
-          elevation: 2,
-          shape: RoundedRectangleBorder(
-              side: BorderSide(color: AppColor.gray.withOpacity(0.3), width: 1),
-              borderRadius: BorderRadius.all(Radius.circular(16))),
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Image.network(
-                  item.imageUrl,
-                  width: 150,
-                  height: 200,
+      child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 250),
+          child: Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                      color: AppColor.gray.withOpacity(0.3), width: 1),
+                  borderRadius: BorderRadius.all(Radius.circular(16))),
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Image.network(
+                      item.imageUrl,
+                      width: 150,
+                      height: 200,
+                    ),
+                    Text(
+                      item.name,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          color: AppColor.green, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      MoneyFormatHelper.formatVNCurrency(
+                          item.price * CurrencyRate.vnd),
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          color: Colors.red, fontWeight: FontWeight.w400),
+                    )
+                  ],
                 ),
-                Text(
-                  item.name,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                      color: AppColor.green, fontWeight: FontWeight.w500),
-                ),
-                Text(
-                  MoneyFormatHelper.formatVNCurrency(
-                      item.price * CurrencyRate.vnd),
-                  overflow: TextOverflow.ellipsis,
-                  style:
-                      TextStyle(color: Colors.red, fontWeight: FontWeight.w400),
-                )
-              ],
-            ),
-          )));
+              ))));
 }
