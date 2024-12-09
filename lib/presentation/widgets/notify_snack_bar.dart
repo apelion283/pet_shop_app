7a16138831079d1c1a4016f654f099bcc6a2605a
Nguyễn Đình Nhật <nhat.nd283@gmail.com>
2024-12-09 09:11:48 +0700

    Add notify snackbar for UX optimize

--- lib/presentation/widgets/notify_snack_bar.dart
+++ lib/presentation/widgets/notify_snack_bar.dart
@@ -2,21 +2,22 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pet_shop_app/core/resources/color_manager.dart';
 
-SnackBar notifySnackBar(String message, Function onHideSnackBarButtonClick) {
+SnackBar notifySnackBar(
+    {required String message, required Function onHideSnackBarButtonClick}) {
   return SnackBar(
     content: Text(message,
         style: TextStyle(
-          color: AppColor.white,
-        )).tr(),
+          color: AppColor.black,
+        )),
     backgroundColor: AppColor.green,
     behavior: SnackBarBehavior.floating,
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(10),
     ),
-    duration: Duration(seconds: 2),
+    duration: Duration(seconds: 1),
     action: SnackBarAction(
-        label: Text('hide').tr().data!,
-        textColor: AppColor.white,
+        label: "hide".tr(),
+        textColor: AppColor.black,
         onPressed: () {
           onHideSnackBarButtonClick();
         }),
